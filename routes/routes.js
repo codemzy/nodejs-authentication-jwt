c022927572b6622a994f61b7eb17b7093d8130f7
Emma J <codemzy@gmail.com>
2016-08-22 16:13:00 +0000

    Added authentication controller and wired up to routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,12 @@
 'use strict';
 
+// controllers
+const Authentication = require('../controllers/authentication');
+
 module.exports = function (app) {
     
-    // weather api route
-    app.route('/')
-        .get(function(req, res) {
-            res.json({ Hello: "world" });
-        });
+    // 
+    app.route('/signup')
+        .post(Authentication.signup);
     
 };
